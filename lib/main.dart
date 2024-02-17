a22456741dd088d9c22e41c19cc4db11ad031b91
tanishqkakkar <kakkartanishq13@gmail.com>
2024-02-18 04:10:58 +0530

    develop login page

--- lib/main.dart
+++ lib/main.dart
@@ -219,7 +219,7 @@ class _LoginState extends State<Login> {
                             elevation: 5,
                             borderRadius: BorderRadius.circular(10),
                             child: Image.asset(
-                              "assets/logo.png",
+                              "assets/google.png",
                               height: 50,
                               width: 90,
                               fit: BoxFit.contain,
